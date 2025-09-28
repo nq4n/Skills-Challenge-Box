db87cfeac102aace46dad969da3c31db086f228c
nq4n <106165111+nq4n@users.noreply.github.com>
2025-09-29 00:12:55 +0400

    utils editing

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 import json
 import os
 from datetime import datetime
-from lib.card_generator import batch_generate_cards
+from utils.card_generator import batch_generate_cards
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
